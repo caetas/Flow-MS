92d2d7cbd666d58cd5581d210f9c99ba8d43e2e6
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2024-09-09 13:45:28 +0200

    eval on cityscapes

--- src/flowms/eval.py
+++ src/flowms/eval.py
@@ -11,6 +11,8 @@ elif args.dataset == 'brats':
     testloader = test_loader_brats(batch_size=args.n_samples, size=args.size)
 elif args.dataset == 'celeb':
     testloader = test_loader_celebamaskhq(batch_size=args.n_samples, size=args.size)
+elif args.dataset == 'cityscapes':
+    testloader = test_loader_cityscapes(batch_size=args.n_samples, size=args.size)
 
 model = FlowMS(args)
 model.load_model(args.checkpoint)
